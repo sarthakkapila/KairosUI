e93c5c13e7b1375078873ee45af911522e218884
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-08-03 16:35:52 +0530

    HEader+FOOTer

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/dark.ico";
+import GithubIcon from "./github-icon";
+
+export default function Header() {
+  return (
+    <header className="relative mx-auto mt-5 flex w-full items-center justify-center px-2 pb-7 sm:px-4">
+      <Link href="/" className="absolute flex items-center gap-2">
+        <Image alt="header text" src={logo} className="h-5 w-5" />
+        <h1 className="text-xl tracking-tight">
+          <span className="text-neon-pink font-extrabold">KairosUI</span>UI
+        </h1>
+      </Link>
+      <a
+        href="https://github.com/sarthakkapila/KairosUI"
+        target="_blank"
+        className="ml-auto hidden items-center gap-3 rounded-2xl bg-white px-6 py-2 sm:flex"
+      >
+        <GithubIcon className="h-4 w-4" />
+        <span>GitHub ⭐️</span>
+      </a>
+    </header>
+  );
+}
