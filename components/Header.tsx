e3fcb21508d6b0288b29262d3b37ae1966ab5489
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-08-14 05:02:41 +0530

    Feat: USerbadge

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../public/dark.ico";
 import GithubIcon from "./github-icon";
-
+import { UserButton, useUser } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
 export default function Header() {
+  const { user } = useUser();
+
+  if (!user) return null;
   return (
     <header className="relative mx-auto mt-5 flex w-full items-center justify-center px-2 pb-7 sm:px-4">
+<div> 
+        <UserButton
+      appearance={ {
+          
+        baseTheme: dark,
+        elements: {
+          avatarBox: "h-14 w-14",
+          userButtonOuterIdentifier: "text-sm font-semibold text-white",
+          userButtonPopoverCard: "bg-white shadow-lg rounded-lg border border-gray-200",
+          userButtonPopoverFooter: "hidden",
+        },
+      }}
+      showName={false}
+    />
+    </div>
       <Link href="/" className="absolute flex items-center gap-2">
         <Image alt="header text" src={logo} className="h-8 w-8" />
         <h1 className="text-xl tracking-tight">
