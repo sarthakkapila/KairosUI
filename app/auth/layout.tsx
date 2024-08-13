0cfbdf398a63df6a2a2dd3d9f23b0dc83002f740
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-08-14 03:56:05 +0530

    Auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -18,25 +18,25 @@ const Layout = async({ children }: Props) => {
     };
   }  return (
     <> 
-      <div className="h-screen text-white flex flex-col justify-center items-center w-full">
-        <div className="flex flex-row items-center p-6">
-          <Image
-            src="/dark.ico"
-            alt="LOGO"
-            width={40}
-            height={40}
-          />
-          <p className='pt-1 pl-2 text-4xl font-black'>KairosUI</p>
-        </div>
-        <div className="text-white flex flex-col items-center text-center">
-          <h2 className="text-gravel md:text-4xl font-extralight">
-            Hi, I’m KairosUI! <br/> AI Frontend Developer
-          </h2>
-        </div>
+      {/* <div className="h-screen text-white flex flex-col justify-center items-center w-full"> */}
+      {/*   <div className="flex flex-row items-center p-6"> */}
+      {/*     <Image */}
+      {/*       src="/dark.ico" */}
+      {/*       alt="LOGO" */}
+      {/*       width={40} */}
+      {/*       height={40} */}
+      {/*     /> */}
+      {/*     <p className='pt-1 pl-2 text-4xl font-black'>KairosUI</p> */}
+      {/*   </div> */}
+      {/*   <div className="text-white flex flex-col items-center text-center"> */}
+      {/*     <h2 className="text-gravel md:text-4xl font-extralight"> */}
+      {/*       Hi, I’m KairosUI! <br/> AI Frontend Developer */}
+      {/*     </h2> */}
+      {/*   </div> */}
         <div className="text-white flex flex-col items-center w-full pt-8">
           {children}
         </div>
-      </div>
+      {/* </div> */}
     </>
   );
 }
